e10dae8686251290671da7a3d67108506d5f662d
Z E E S H A N <mzeeshanayaz685@gmail.com>
2025-07-24 17:31:06 +0500

    Q6

---
+++ Lab 03/Q6/Student.java
@@ -0,0 +1,7 @@
+package Q6;
+
+public class Student {
+    String name;
+    int age;
+    String CourseEnrolled;
+}
---
+++ Lab 03/Q6/main.java
@@ -0,0 +1,12 @@
+package Q6;
+
+public class main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name="Muhammad Zeeshan Ayaz";
+        s1.age=18;
+        s1.CourseEnrolled= "OOP in JAVA";
+        System.out.println("My name is " + s1.name+",I'm "+s1.age+" years old"+",and I am studying "+s1.CourseEnrolled);
+
+    }
+}
